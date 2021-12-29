e2d296b45e9954265d4daa83ed993d3d49a329c0
fanchangyong <fanchangyong@gmail.com>
2021-12-29 19:20:33 +0800

    Add speech

--- Shared/PhraseDetailView.swift
+++ Shared/PhraseDetailView.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct PhraseDetailView: View {
     var phrase: Phrase
+    let synthesizer = AVSpeechSynthesizer()
     
     var body: some View {
         ScrollView {
@@ -21,17 +23,31 @@ struct PhraseDetailView: View {
                 ProgressView()
             }
             
-            VStack(alignment: .leading, spacing: 8) {
-                Text(phrase.title)
-                    .font(.title)
-                    .fontWeight(.bold)
+            VStack(alignment: .leading, spacing: 24) {
+                HStack() {
+                    Text("\(phrase.title)")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
                 Text(phrase.desc ?? "")
                 Text("例句")
                     .font(.system(size: 24))
                     .fontWeight(.medium)
                 
                 ForEach(phrase.sentences ?? [], id: \.self) { sentence in
-                    Text(sentence)
+                    VStack(alignment: .leading, spacing: 8) {
+                        Button("🗣Speak") {
+                            print("clicked")
+                            if synthesizer.isSpeaking {
+                                synthesizer.stopSpeaking(at: .immediate)
+                            }
+                            let utterance = AVSpeechUtterance(string: sentence)
+                            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                            synthesizer.speak(utterance)
+                        }
+                        Text(sentence)
+                        
+                    }
                 }
                 
             }
